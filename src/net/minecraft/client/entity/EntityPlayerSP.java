8459f9aba79398c39bf5e6c6ada54d60e4b20526
TIMER-err <zqq2333@outlook.com>
2023-09-10 09:54:52 +0800

    fix

--- src/net/minecraft/client/entity/EntityPlayerSP.java
+++ src/net/minecraft/client/entity/EntityPlayerSP.java
@@ -1,23 +1,13 @@
 package net.minecraft.client.entity;
 
+import cn.timer.isense.event.EventManager;
+import cn.timer.isense.event.events.EventPostUpdate;
+import cn.timer.isense.event.events.EventPreUpdate;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.MovingSoundMinecartRiding;
 import net.minecraft.client.audio.PositionedSoundRecord;
-import net.minecraft.client.gui.GuiCommandBlock;
-import net.minecraft.client.gui.GuiEnchantment;
-import net.minecraft.client.gui.GuiHopper;
-import net.minecraft.client.gui.GuiMerchant;
-import net.minecraft.client.gui.GuiRepair;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.GuiScreenBook;
-import net.minecraft.client.gui.inventory.GuiBeacon;
-import net.minecraft.client.gui.inventory.GuiBrewingStand;
-import net.minecraft.client.gui.inventory.GuiChest;
-import net.minecraft.client.gui.inventory.GuiCrafting;
-import net.minecraft.client.gui.inventory.GuiDispenser;
-import net.minecraft.client.gui.inventory.GuiEditSign;
-import net.minecraft.client.gui.inventory.GuiFurnace;
-import net.minecraft.client.gui.inventory.GuiScreenHorseInventory;
+import net.minecraft.client.gui.*;
+import net.minecraft.client.gui.inventory.*;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.command.server.CommandBlockLogic;
 import net.minecraft.entity.Entity;
@@ -29,27 +19,12 @@ import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.play.client.C01PacketChatMessage;
-import net.minecraft.network.play.client.C03PacketPlayer;
-import net.minecraft.network.play.client.C07PacketPlayerDigging;
-import net.minecraft.network.play.client.C0APacketAnimation;
-import net.minecraft.network.play.client.C0BPacketEntityAction;
-import net.minecraft.network.play.client.C0CPacketInput;
-import net.minecraft.network.play.client.C0DPacketCloseWindow;
-import net.minecraft.network.play.client.C13PacketPlayerAbilities;
-import net.minecraft.network.play.client.C16PacketClientStatus;
+import net.minecraft.network.play.client.*;
 import net.minecraft.potion.Potion;
 import net.minecraft.stats.StatBase;
 import net.minecraft.stats.StatFileWriter;
 import net.minecraft.tileentity.TileEntitySign;
-import net.minecraft.util.BlockPos;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.IChatComponent;
-import net.minecraft.util.MovementInput;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.*;
 import net.minecraft.world.IInteractionObject;
 import net.minecraft.world.World;
 import pisi.unitedmeows.minecraft.Settings;
@@ -163,6 +138,7 @@ public class EntityPlayerSP extends AbstractClientPlayer {
 	 * movement.
 	 */
 	public void onUpdateWalkingPlayer() {
+        EventManager.instance.call(new EventPreUpdate());
 		final boolean flag = this.isSprinting();
 		if (mc.gameSettings.keyBindAttack.isKeyDown() && mc.thePlayer.isUsingItem() && Settings.OLD_ANIMATIONS)
 			if (mc.objectMouseOver != null && mc.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) if (mc.thePlayer != null) mc.thePlayer.clientSideSwing();
@@ -206,6 +182,7 @@ public class EntityPlayerSP extends AbstractClientPlayer {
 				this.lastReportedPitch = this.rotationPitch;
 			}
 		}
+        EventManager.instance.call(new EventPostUpdate());
 	}
 
 	/**
